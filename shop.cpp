71e2516d589360fab29ba709e62063b91c938e7a
Pham Tien Dat <ptdat2422@apcs.fitus.edu.vn>
2024-12-04 17:01:01 +0700

    add function definition for Shop::addItem()

--- shop.cpp
+++ shop.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <fstream>
+#include <cstring>
 using namespace std;
 
 #include "ShopManagement.h"
@@ -7,6 +8,26 @@ using namespace std;
 void Shop::addItem() {
 	f.open("database.txt", ios::out | ios::binary);
 	if (f.is_open()) {
+		Item i;
 
+		cout << "ID: ";
+		cin >> i.ID;
+		cin.ignore();
+
+		char* tmpName = new char[100];
+		cout << "Name: ";
+		cin.get(tmpName, 100, '\n');
+		cin.ignore();
+		i.name = new char[strlen(tmpName) + 1];
+		strcpy_s(i.name, strlen(tmpName) + 1, tmpName);
+		delete[] tmpName;
+
+		cout << "Price: ";
+		cin >> i.price;
+
+		f.write((char*)&i, sizeof(Item));
 	}
+	else cout << "Error" << endl;
+
+	f.close();
 }
