355ab0df766df103a8cbb7e38fd89c2349016266
Minh Quan <nmquan2420@apcs.fitus.edu.vn>
2024-12-04 16:25:46 +0700

    ShopManagement.h is created

---
+++ ShopManagement.h
@@ -0,0 +1,27 @@
+#pragma once
+#include <iostream>
+using namespace std;
+
+struct Item {
+	int ID;
+	char* name;
+	float price;
+
+	void input() {
+		cout << "Enter item's ID: "; cin >> ID;
+		cout << "Enter item's name: ";
+		char* tmp = new char[50];
+		cin.ignore();
+		cin.get(tmp, 50);
+		name = new char[(strlen(tmp) + 1)];
+		strncpy_s(name, strlen(tmp) + 1, tmp, strlen(tmp) + 1);
+		delete[] tmp;
+		cout << "Enter item's price: "; cin >> price;
+	}
+
+	void display() const {
+		cout << "Item's ID : " << ID << endl;
+		cout << "Item's name: " << name << endl;
+		cout << "Item's price: " << price << endl;
+	}
+};
