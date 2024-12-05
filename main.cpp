42c4a6b83c9bc7d9bced670c16337fcfa3c86f62
Pham Tien Dat <ptdat2422@apcs.fitus.edu.vn>
2024-12-05 14:22:28 +0700

    aa

--- main.cpp
+++ main.cpp
@@ -1,7 +1,14 @@
 #include <iostream>
+#include <fstream>
 using namespace std;
 
+#include "ShopManagement.h"
+
 int main() {
+	fstream f("database.bin", ios::out | ios::binary);
 	Menu m;
 
+	m.menu();
+
+	return 0;
 }
